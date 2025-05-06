68e915a7285da042b7738e2a48d147fe29860fbe
DouglasMacKrell <d.mackrell@gmail.com>
2025-05-05 20:28:28 -0400

    NGN-003: Fix mypy type errors and format code

--- namegnome/tests/test_version.py
+++ namegnome/tests/test_version.py
@@ -3,7 +3,7 @@
 from namegnome import __version__
 
 
-def test_version():
+def test_version() -> None:
     """Test that version is a string."""
     assert isinstance(__version__, str)
     assert len(__version__) > 0
